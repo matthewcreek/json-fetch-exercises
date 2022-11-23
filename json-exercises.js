382ac85b9521630d74204e7319bb5e5ba0c468fe
Matthew Creek <matthew.creek27@gmail.com>
2022-11-23 16:03:43 -0600

    added answers to json exercises to repo

---
+++ json-exercises.js
@@ -0,0 +1,5 @@
+// answers for json exercises:
+
+// 1. b. is correct, a. doesn't have double quotes around the keys, c. doesn't have double quotes around the values
+
+// 2. a. is correct, b. doesn't have a key for the nested object array, c. is an array not nested within an object
